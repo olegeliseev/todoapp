e826bdea82de312949aed9a6445cacf213f37596
olegeliseev <olegeliseev96@gmail.com>
2025-02-07 15:15:36 +0300

    Создана модель Task, миграция и основной контроллер

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [MainController::class, 'index']);
+Route::post('/tasks', [MainController::class, 'store']);
+Route::put('/tasks/{id}', [MainController::class, 'update']);
